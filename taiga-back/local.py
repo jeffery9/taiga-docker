8f7277c22439b8d02a537d2ea940a4d4a2721f6c
jeffery <jeffery9@gmail.com>
2020-11-21 20:12:08 +0800

    support events and upgrade to 5.5.7

--- taiga-back/local.py
+++ taiga-back/local.py
@@ -2,6 +2,11 @@ from .common import *
 
 from .dockerenv import *
 
+SECRET_KEY = '9%pno@m688el28@2+^y4v^&6wluqk-g#j#d7$dsjtht)o30dn1'
+
+EVENTS_PUSH_BACKEND = "taiga.events.backends.rabbitmq.EventsPushBackend"
+EVENTS_PUSH_BACKEND_OPTIONS = {"url": "amqp://guest:guest@broker/taiga"}
+
 # THROTTLING
 #REST_FRAMEWORK["DEFAULT_THROTTLE_RATES"] = {
 #    "anon": "20/min",
